6ae2d0984ceeb323a440542067a83c2388df4f24
宋波伟 <songbowei211119@credithc.com>
2025-10-23 17:43:14 +0800

    Supports the situation where the '英文' column already exists

--- code_heal_eng/Backend/merge_eng.py
+++ code_heal_eng/Backend/merge_eng.py
@@ -1,10 +1,13 @@
 import pandas as pd
 
-file_name = '时态作业3_1.xlsx'
+file_name = '陈述句作业3.xlsx'
 processed_file_name = '{}_转换.csv'.format(file_name[:-5])
 
 # 读取Excel（跳过标题行）
 df = pd.read_excel(file_name, sheet_name='Sheet1', header=1)
+if '中文' not in df.columns:
+    # 对于某些 excel，第一行就是标题
+    df = pd.read_excel(file_name, sheet_name='Sheet1')
 
 # 生成英文句子
 def generate_english(row):
@@ -15,8 +18,15 @@ def generate_english(row):
     return ' '.join(parts).capitalize() + '?'
 
 # 处理数据
-df['英文'] = df.apply(generate_english, axis=1)
-result_df = df[['中文', '时态', '英文']] if '时态' in df.columns else df[['中文', '英文']]
+if '时态' in df.columns:
+    result_df = df[['中文', '时态', '英文']]
+# '提示' 这一列是我手动加入到原始 excel中的，
+#  因为比如 陈述句作业3.xlsx 中，会有一些提示，比如这句话是表示能力，表示推测等
+elif '提示' in df.columns:
+    result_df = df[['提示', '中文', '英文']]
+else:
+    result_df = df[['中文', '英文']]
+
 
 # Process unnecessary lines and merge lines with '中文' field empty
 # 1. 删除英文字段为 "?" 的行
@@ -30,6 +40,10 @@ for i in range(1, len(df)):
         if pd.notna(curr_en):         # 如果当前行的英文不为空
             df.loc[i-1, '英文'] = f"{prev_en} | {curr_en}"  # 合并英文
         df = df.drop(i)  # 删除当前行
+    # 删除中文是 '中文'的句子
+    elif df.loc[i, '中文'] == '中文':
+        df = df.drop(i)
+
 
 # 重置索引
 df = df.reset_index(drop=True)
